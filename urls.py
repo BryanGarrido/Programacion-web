7842832d2cfb5467c0c42fa148d022eba15c2e46
sevelido parada <bry.garrido@duocuc.cl>
2024-06-24 21:49:47 -0400

    data prod

--- urls.py
+++ urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alumnos/',include('alumnos.urls'))
+    path('alumnos/', include('alumnos.urls'))
+    
 ]
